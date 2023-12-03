891c0dfa37a0d82fda824efdd7bebe3ea15e4a8a
alevargasa90 <alevargasa90@live.seminolestate.edu>
2023-12-03 18:42:15 -0500

    added 3 test more

--- src/test/java/com/vargas/AppTest.java
+++ src/test/java/com/vargas/AppTest.java
@@ -14,8 +14,7 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue(){
         assertTrue( true );
     }
     
@@ -23,8 +22,23 @@ public class AppTest
     public void fibonacciOf10Is55() {
 
         assertEquals(55,App.calculateFibonacci(10));
-
     }
     
-    
+    @Test
+    public void fibonacciOf5Is5() {
+
+        assertEquals(5,App.calculateFibonacci(5));
+
+    }
+
+    @Test
+    public void fibonacciOf0Is() {
+
+        assertEquals(0,App.calculateFibonacci(0));
+
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void fibonacciOfNegative() {
+        assertEquals(0 ,App.calculateFibonacci(-1));
+    }    
 }
