9d326d02922c8fa3f74955a25f09c06274ebc55a
alevargasa90 <alevargasa90@live.seminolestate.edu>
2023-12-05 15:44:08 -0500

    changes on the app so it calculates the Fibo on demand

--- src/main/java/com/vargas/App.java
+++ src/main/java/com/vargas/App.java
@@ -2,44 +2,41 @@ package com.vargas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Hello world!
- *
- */
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-@RestController
+@Controller
+public class App {
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(App.class, args);
-    	
-     //   System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
-    @GetMapping("/hellopipeline")
-    public String mavenRest() {
-    	
-    	final int n = 8;
-    	 int fibo = calculateFibonacci(n);
-           
-    	
-    	return "Hello from the pipeline and Git " + fibo;
+
+    @GetMapping("/menu")
+    public String menu() {
+        return "menu";
     }
-	static int calculateFibonacci(int n) {
-		// TODO Auto-generated method stub
-		 if (n < 0) {
-             throw new IllegalArgumentException("n must be a positive integer.");
-         }	else if  (n == 0) {
-         	return 0;
-         } else if (n == 1) {
-             return 1;
-         } else {
-             return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
-         }
-	}
+
+    @PostMapping("/calculateFibonacci")
+    public String calculateFibonacci(@RequestParam(value = "n", defaultValue = "0") int n, Model model) {
+        int fibo = calculateFibonacciRecursive(n);
+        model.addAttribute("result", "The Fibonacci sequence for n=" + n + " is " + fibo);
+        return "menu";
+    }
+
+     static int calculateFibonacciRecursive(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be a non-negative integer.");
+        } else if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return calculateFibonacciRecursive(n - 1) + calculateFibonacciRecursive(n - 2);
+        }
+    }
 }
--- src/test/java/com/vargas/AppTest.java
+++ src/test/java/com/vargas/AppTest.java
@@ -21,24 +21,24 @@ public class AppTest
     @Test
     public void fibonacciOf10Is55() {
 
-        assertEquals(55,App.calculateFibonacci(10));
+        assertEquals(55,App.calculateFibonacciRecursive(10));
     }
     
     @Test
     public void fibonacciOf5Is5() {
 
-        assertEquals(5,App.calculateFibonacci(5));
+        assertEquals(5,App.calculateFibonacciRecursive(5));
 
     }
 
     @Test
     public void fibonacciOf0Is() {
 
-        assertEquals(0,App.calculateFibonacci(0));
+        assertEquals(0,App.calculateFibonacciRecursive(0));
 
     }
     @Test(expected = IllegalArgumentException.class)
     public void fibonacciOfNegative() {
-        assertEquals(0 ,App.calculateFibonacci(-1));
+        assertEquals(0 ,App.calculateFibonacciRecursive(-1));
     }    
 }
