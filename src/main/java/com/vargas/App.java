4e58c4d72dfd22bdf253e955986e35427c5920a8
alevargasa90 <alevargasa90@live.seminolestate.edu>
2023-12-04 21:16:43 -0500

    add hehe to the text

--- src/main/java/com/vargas/App.java
+++ src/main/java/com/vargas/App.java
@@ -28,7 +28,7 @@ public class App
     	 int fibo = calculateFibonacci(n);
            
     	
-    	return "Hello from the pipeline and Git " + fibo;
+    	return "Hello from the pipeline and Git hehe " + fibo;
     }
 	static int calculateFibonacci(int n) {
 		// TODO Auto-generated method stub
